5a234cd7f5bee07f850f865dffd834617d301d3a
Floriana <floriana.doko@hoyo.tech>
2025-02-05 17:17:56 +0100

    removed unneccesary code

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,6 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   webpack: (config) => {
-//     // Ensure Babel is applied correctly
-//     config.module.rules.push({
-//       test: /\.(ts|tsx)$/,
-//       use: {
-//         loader: "babel-loader",
-//         options: {
-//           presets: ["next/babel"],
-//         },
-//       },
-//     });
-
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* Remove Babel-related settings */
   webpack: (config) => {
     return config;
   },
